4faddd34c59ef96694fbcaface085737b46bf5bc
Rex Terminous <rizwan.raza987@gmail.com>
2019-09-04 18:06:52 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'src/app.dart';
+import 'package:flutter/material.dart';
+
+main(List<String> args) {
+  runApp(App());
+}
---
+++ lib/src/resources/data_provider.dart
@@ -0,0 +1,8 @@
+import 'package:http/http.dart';
+
+class DataProvider {
+  Client client = Client();
+  getList() {
+    return null;
+  }
+}
